6840b56d1feb47cc803d7478f6b655c206b71527
Arvindraj73 <arvindraj3172@gmail.com>
2021-04-29 15:10:31 +0530

    removed code

--- sample-app/src/components/BookForm.js
+++ sample-app/src/components/BookForm.js
@@ -9,23 +9,18 @@ function BookForm() {
 
     function isDigit(number) {
         let exp = /\D/;
-        return exp.test(number);
+        return exp.test(number)
     }
     //handler to add book on button click
     const addBook = (event) => {
         event.preventDefault();
-        if (bookName.length === 0 && isDigit(bookNumber)) {
-            
-        }
-        else {
-            setBooks([
-                ...books,
-                {
-                    sno: bookNumber,
-                    name: bookName
-                }
-            ]);
-        }
+        setBooks([
+            ...books,
+            {
+                sno: bookNumber,
+                name: bookName
+            }
+        ]);
     }
 
     const changeBookNumber = (e) => {
@@ -41,9 +36,9 @@ function BookForm() {
     return (
         <div>
             <form onSubmit={addBook}>
-                <input type="text" name="booknumber" placeholder="Book Number" onChange={changeBookNumber} required/><br />
+                <input type="text" name="booknumber" placeholder="Book Number" onChange={changeBookNumber} required /><br />
                 {bookNumberError ? <span style={{ color: "red" }}>Please Enter a valid Number</span> : ''}<br />
-                <input type="text" name="bookname" placeholder="Book Name" onChange={e => setBookName(e.target.value)} required/><br />
+                <input type="text" name="bookname" placeholder="Book Name" onChange={e => setBookName(e.target.value)} required /><br />
                 <input type="submit" value="Add Book" />
             </form>
             <Table books={books} />
